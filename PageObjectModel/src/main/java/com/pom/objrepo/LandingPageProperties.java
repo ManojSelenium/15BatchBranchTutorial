c2d76698e9e7f956c69aeb305b7030451fb4732d
ManojSelenium <manojqa5006@gmail.com>
2018-12-10 08:53:55 +0530

    SecondBranchCommit

--- PageObjectModel/src/main/java/com/pom/objrepo/LandingPageProperties.java
+++ PageObjectModel/src/main/java/com/pom/objrepo/LandingPageProperties.java
@@ -5,5 +5,6 @@ import org.openqa.selenium.By;
 public interface LandingPageProperties {
 
 	By SIGNIN_LOCATOR=By.xpath("//a[@class='login']");
+	By SIGNIN_LOCATOR1=By.xpath("//a[@class='login']");
 	By CONTACTUS_LOCATOR=By.xpath("//a[@class='login']");
 }
